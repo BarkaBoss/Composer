55a5016c71999a1f689d0bb90497c38dc93d242e
BarkaBoss <bossmajor.biii@gmail.com>
2022-09-19 00:09:24 +0100

    Added Image View

--- app/src/main/java/ng/com/nokt/composer/MainActivity.kt
+++ app/src/main/java/ng/com/nokt/composer/MainActivity.kt
@@ -1,20 +1,34 @@
 package ng.com.nokt.composer
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ng.com.nokt.composer.ui.theme.ComposerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContent {
             ComposerTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    HomeHeader()
                 }
             }
         }
@@ -30,14 +44,91 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomeHeader(){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+    ) {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.QueueMusic,
+                contentDescription = "Company Logo",
+                tint = Color.Gray
+            )
+            Icon(
+                imageVector = Icons.Rounded.Favorite,
+                contentDescription = "Favourite",
+                tint = Color.Gray
+            )
+        }
+
+        MusicArt()
+
+        Spacer(Modifier.height(10.dp))
+
+        SongArtistTitle()
+        //Spacer(Modifier.height(10.dp))
+    }
+
+
+
+}
+
+@Composable
+fun MusicArt(){
+    Column(
+        modifier = Modifier
+            .height(350.dp)
+            .fillMaxWidth()
+            .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Card(modifier = Modifier
+            .height(350.dp)
+            .width(350.dp)
+            .clip(RoundedCornerShape(corner = CornerSize(30.dp)))) {
+            Image(painter = painterResource(id = R.drawable.nas),
+                contentDescription = "Album Art",
+                contentScale = ContentScale.Crop,
+                //modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+}
+
+@Composable
+fun SongArtistTitle(){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Count Your Blessings",
+            style = TextStyle(
+                fontWeight = FontWeight.SemiBold,
+                color = Color.DarkGray,
+                fontSize = 24.sp
+            )
+        )
+        Spacer(Modifier.height(5.dp))
+        Text(text = "Artist's Name",
+            style = TextStyle(
+                color = Color.Gray,
+                fontSize = 14.sp
+            )
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposerTheme {
-        Greeting("Android")
+        HomeHeader()
     }
 }
